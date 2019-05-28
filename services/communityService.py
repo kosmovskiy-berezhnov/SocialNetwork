def2028142b466cd2951644ac9c3b315a1e27005
nik1998 <beregnovnikita@gmail.com>
2019-05-28 13:12:38 +0300

    communities

--- services/communityService.py
+++ services/communityService.py
@@ -6,16 +6,18 @@ mod = Blueprint('community', __name__)
 from app import db
 from models.user import User
 from models.community import Community
+from models.post import Post
 from models.moderator import Moderator
+
 
 @mod.route('/mycommunities', methods=['GET'])
 def mycommunities():
     query = db.session.query(User).filter_by(username=session['name']).join(User.user_subscribe)
     data = query.first()
     if data != None:
-        data =data.user_subscribe
+        data = data.user_subscribe
     else:
-        data =[]
+        data = []
     return render_template('mycommunities.html', communities=data)
 
 
@@ -46,3 +48,36 @@ def createcommunity():
     session['com_id'] = newcommunity.id
     db.session.commit()
     return redirect('/mycommunities')
+
+
+@mod.route('/subscribecommunity', methods=['GET'])
+def subscribecommunity():
+    nuser = User.query.filter_by(id=session['user_id']).one()
+    community = Community.query.filter_by(id=session['com_id']).first()
+    community.subscribe_user.append(nuser)
+    db.session.add(community)
+    db.session.commit()
+    return redirect('/community')
+
+
+@mod.route('/community', methods=['GET'])
+def community():
+    st = request.args.get('val', '')
+    if st != '':
+        session['com_id'] = st
+    community = Community.query.filter_by(id=session['com_id']).join(Community.community_posts, isouter=True).first()
+    return render_template('community.html', posts=community.community_posts, com=community)
+
+
+@mod.route('/addpost', methods=['GET','POST'])
+def addpost():
+    if request.method == 'POST':
+        id = request.form['id']
+        community = Community.query.filter_by(id=session['com_id']).first()
+        post = Post.query.filter_by(id=id).first()
+        community.community_posts.append(post)
+        db.session.add(community)
+        db.session.commit()
+        return redirect('/community')
+    data = Post.query.filter_by(author=session['name']).order_by(Post.creation_date.desc()).all()
+    return render_template("addpost.html", posts=data)
